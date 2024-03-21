f9817946b1dcc8ca0f81fcc46342b084ef0be20a
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 02:28:01 +0500

    Add files via upload
    THIS IS MY PROJECT

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict";
+let alienColors = "green";
+if (alienColors === "green") {
+    console.log("Player just earned 5 point for shooting is alien.");
+}
+else {
+    console.log("Player just earned 10 point.");
+}
+alienColors = "yellow";
+if (alienColors === "green") {
+    console.log("Player just earned 5 point for shooting is alien.");
+}
+else {
+    console.log("Player just earned 10 point.");
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+
+
+let alienColors :string ="green";
+
+if(alienColors === "green"){
+    console.log("Player just earned 5 point for shooting is alien.");
+}else{
+    console.log("Player just earned 10 point.");
+}
+
+alienColors ="yellow";
+
+if(alienColors === "green"){
+    console.log("Player just earned 5 point for shooting is alien.");
+}else{
+    console.log("Player just earned 10 point.");
+}
